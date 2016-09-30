a34911fb93ec8b5039cb48255a903e4801184764
RaysChen <543114160@qq.com>
2016-09-30 16:41:03 +0800

    Thread
    两个常用的gcd函数

---
+++ GCD常用函数1/GCD常用函数1Tests/GCD____1Tests.m
@@ -0,0 +1,39 @@
+//
+//  GCD____1Tests.m
+//  GCD常用函数1Tests
+//
+//  Created by 陈曦 on 16/9/30.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GCD____1Tests : XCTestCase
+
+@end
+
+@implementation GCD____1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
