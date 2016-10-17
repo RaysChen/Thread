f9b26dad7d04c8e931a2ece8eea459564a101aa9
RaysChen <543114160@qq.com>
2016-10-17 17:34:14 +0800

    Thread
    框架使用的小练习

---
+++ SDWebImage使用练习/SDWebImage使用练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDWebImage使用练习
+//
+//  Created by 陈曦 on 16/10/17.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
